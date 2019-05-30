1eb9bc70c361315a5644b622bb97fc024a4cf492
Kenny Osele <oseleonose@gmail.com>
2019-05-29 21:12:51 -0400

    Reverse words codewar completed

---
+++ Reverse.py
@@ -0,0 +1,26 @@
+def reverse_words(text):
+    newWords = []
+    words = text.split(' ')
+    for word in words:
+        letters = [char for char in word]
+        letters.reverse()
+        newWords.append(''.join(letters))
+    return ' '.join(newWords)
+
+#codewar best solution
+def reverse_words2(text):
+    return ' '.join(s[::-1] for s in str.split(' '))
+
+import unittest
+class TestReverse(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(reverse_words('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god')
+    def test_2(self):
+        self.assertEqual(reverse_words('apple'), 'elppa')
+    def test_3(self):
+        self.assertEqual(reverse_words('a b c d'), 'a b c d')
+    def test_4(self):
+        self.assertEqual(reverse_words('double  spaced  words'), 'elbuod  decaps  sdrow')
+
+if __name__ == '__main__':
+    unittest.main()
